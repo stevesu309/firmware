519f783a7d256b584741ae9d448040e33beaa7fc
steve.su <steve.su@sainstore.com>
2026-01-21 10:36:25 +0800

    UPDATE：LORA引脚定义

--- variants/esp32s3/red_bank_s3/variant.h
+++ variants/esp32s3/red_bank_s3/variant.h
@@ -3,7 +3,7 @@
 // #define I2C_SDA 5
 // #define I2C_SCL 4
 #define USE_SX1262
-#define USE_SX1268
+// #define USE_SX1268
 
 #define HAS_WIRE 0 // 没有iic设备
 
@@ -18,18 +18,15 @@
 #define GPS_TX_PIN 17
 
 // Display (E-Ink)
-// #define PIN_EINK_BS1 4
 #define PIN_EINK_CS 15
 #define PIN_EINK_BUSY 13
 #define PIN_EINK_DC 16
 #define PIN_EINK_RES 12
 #define PIN_EINK_SCLK 14
 #define PIN_EINK_MOSI 11
-// #define EINK_POWER_PIN 38
 
 #define LORA_DIO0 -1 // a No connect on the SX1262 module
 #define LORA_RESET 38
-#define LORA_DIO1 10 // SX1262 IRQ
 
 #define LORA_DIO3 // Not connected on PCB, but internally on the TTGO SX1262, if DIO3 is high the TXCO is enabled
 #define LORA_SCK 5
@@ -43,28 +40,22 @@
 // 实际选择哪个模块由 main.cpp 中的运行时逻辑根据 config.lora.region 决定。
 #define LORA_CS_900 7
 #define LORA_CS_433 4
-#define LORA_ANT 47 // 收发模式切换
+#define LORA_BUSY_433 47 // LoRa 模块电源控制引脚 // 收发模式切换
+#define LORA_DIO1_433 48
 #define LORA_CS LORA_CS_900
+#define LORA_BUSY_900 9
+#define LORA_DIO1_900 10 // SX1262 IRQ
+#define LORA_BUSY LORA_BUSY_900
 
-// 单芯片版本，双匹配电路，通过 LORA_ANT 切换天线，RF_CTRL 控制收发模式
-// #define LORA_ANT 47 // 天线切换
-// #define RF_CTRL 48  // 收发模式切换
-// #define LORA_CS 7
-// #define LORA_DIO2 4       // 单芯片双电路版本
-
-#define SX126X_CS LORA_CS // 作为默认值，实际 cs 在 RED_BANK_S3 中会根据区域动态选择
-#define SX126X_DIO1 10    // LORA_DIO1
-#define SX126X_BUSY 9
+#define SX126X_CS LORA_CS         // 作为默认值，实际 cs 在 RED_BANK_S3 中会根据区域动态选择
+#define SX126X_DIO1 LORA_DIO1_900 // LORA_DIO1
+#define SX126X_BUSY LORA_BUSY
 #define SX126X_RESET LORA_RESET // LORA_RESET
 #define SX126X_DIO2_AS_RF_SWITCH
-#define SX126X_DIO3_TCXO_VOLTAGE 1.8 // 双模块版本-有源
-#define TCXO_OPTIONAL                // 双模块版本-有源
 
 #define PIN_GPS_RESET (39) // GNSS模块复位引脚
 
 #define PIN_LORA_EN 42 // LoRa 模块电源控制引脚
-// #define HAS_SDCARD // Have SPI interface SD card slot
-// #define SDCARD_USE_SPI1
 
 // #define HAS_GPS
 #define PIN_GPS_EN (21)       // GNSS模块电源控制引脚
